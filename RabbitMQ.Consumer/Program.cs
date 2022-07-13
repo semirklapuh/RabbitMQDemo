3c20f4bf08d1f08bf1982adb91587331136aca6e
semirklapuh <semirklapuh@gmail.com>
2022-07-13 15:42:10 +0200

    git init

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQ.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+
+            };
+            using var connection = factory.CreateConnection();
+            using var chanell = connection.CreateModel();
+            chanell.QueueDeclare("demo-queue",
+                durable: true,
+                exclusive: false,
+                arguments: null);
+
+            var consumer = new EventingBasicConsumer(chanell);
+
+            consumer.Received += (sender, e) =>
+            {
+                var body = e.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+            };
+
+            chanell.BasicConsume("demo-queue", true, consumer);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQ.Producer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace RabbitMQ.Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+
+            };
+            using var connection = factory.CreateConnection();
+            using var chanell = connection.CreateModel();
+            chanell.QueueDeclare("demo-queue",
+                durable: true,
+                exclusive: false,
+                arguments: null);
+            var message = new { Name = "Producer", Message = "Hello" };
+            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+            
+            chanell.BasicPublish("","demo-queue", null,body);
+        }
+    }
+}
